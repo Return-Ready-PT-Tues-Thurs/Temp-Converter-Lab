4af8242a3b0b0efbcd46ebfb55eeda32f5e5ca01
Kimberly Ramirez <kimberly.ramirez0313@gmail.com>
2021-01-05 20:54:28 -0500

    add comments

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-console.log('hello');
+
 let finalValue = document.querySelector('.result');
 let button = document.querySelector('button');
 button.addEventListener('click', toCelsius);
 
-function addItems(newNode){
-    celsius.appendChild(newNode);
-}
-
-function toCelsius() 
-{
-//   var fTemp = fahrenh
+function toCelsius() {
 let fahrenheit = parseFloat(document.querySelector('#inputValue').value);
     let result = (fahrenheit - 32) * 5 / 9;
-//   var fToCel = (fTemp - 32) * 5 / 9;
-//   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
-//     console.log(message);
+    //add result to the page
     finalValue.innerHTML = result;
-
 } 
