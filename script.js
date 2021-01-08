b6571c945c0a180ed3ede92ccaed7e231a8c5260
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-07 23:00:18 -0500

    added features for 4 different temp conversions

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-const value = document.getElementsByClassName("fahrenheit");
-console.log(value);
 
 function convert(value) {
    let celsius = (5/9) * (value - 32);
    return celsius;
 }
 
+/*function convert(value) {
+let fahrenheit  = celsius - (32/1.8);
+}*/
+
 const myButton = document.getElementById("myButton");
-console.log(myButton);
-  myButton.addEventListener("click", () => {
-        const fToC = convert(value);
-        const p = document.createElement("p");
-        p.innerHTML("fToC");
-        p.appendChild("p");  
-    });
+
+myButton.addEventListener("click", () => {
+   let input = document.getElementById("fahrenheit").value;
+   /*let input = document.getElementById("celsius").value;*/
+      const fToC = convert(input);
+      document.getElementById("returnValue").textContent = fToC;
+      console.log(fToC);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('convert').onclick = tempConvert;
+// document.getElementById('clear').onclick = clearForm;
+
+// function tempConvert() {
+    
+//     var fahrenheit = document.getElementById("fahrenheit").value;
+//     var celsius = document.getElementById("celsius").value;
+  
+//   if (fahrenheit != '') {
+//         celsius = (parseFloat(fahrenheit) - 32) / 1.8;
+//     } else {
+//         fahrenheit = (parseFloat(celsius) * 1.8) + 32;
+//     }
+  
+
+
+//     document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
+//     document.getElementById('celsius').value = parseFloat(celsius).toFixed(1);
+// }
