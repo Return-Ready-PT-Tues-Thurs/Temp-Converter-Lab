380421e07ff70ea1b90d07b93d9f389354e3dc23
Earl M. Smith <unixsa@gmail.com>
2021-01-09 12:21:45 -0500

    fixed col spacing

---
+++ app.js
@@ -0,0 +1,16 @@
+// function fToC(fahrenheit) {
+//   var fTemp = fahrenheit;
+//   var fToCel = (fTemp - 32) * 5 / 9;
+//   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//     console.log(message);
+// }
+
+
+
+
+// function fToC(fahrenheit) {
+//     document.getElementById("fValue").value;
+//     let fTemp = fahrenheit;
+//     let fToCel = (fTemp - 32) * 5 / 9;
+//     console.log()
+// }
